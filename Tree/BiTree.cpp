0fa94f148230b7f3e194c604c750142f1d3a60e6
OGAS <2970143166@qq.com>
2024-12-06 23:28:59 +0800

    修改了二叉树实现 实现了哈夫曼树

--- Tree/BiTree.cpp
+++ Tree/BiTree.cpp
@@ -2,122 +2,184 @@
 #include <string>
 #include <queue>
 #include <stack>
+#include <limits>
 
 using namespace std;
 
-// 二叉树结点
-class BiTreeNode
+// 二叉树结点类
+template <typename T>
+class BinaryTreeNode
 {
 public:
-    char data;              // 结点存储的数据
-    BiTreeNode *LeftChild;  // 左子树指针
-    BiTreeNode *RightChild; // 右子树指针
-
-    // 构造函数
-    BiTreeNode() : LeftChild(nullptr), RightChild(nullptr) {}
+    T data;
+    BinaryTreeNode<T> *left;  // 左子树指针
+    BinaryTreeNode<T> *right; // 右子树指针
+    BinaryTreeNode() : data(0), left(NULL), right(NULL) {}
 };
 
 // 二叉树类
-class BiTree
+template <typename T>
+class BinaryTree
 {
-private:
-    BiTreeNode *root; // 根节点指针
-    int pos;
-    string strTree;
-
-    BiTreeNode *CreateBiTree();
-    void preOrder(BiTreeNode *t);
-    void inOrder(BiTreeNode *t);
-    void postOrder(BiTreeNode *t);
-
 public:
-    BiTree();
-    void CreateTree(string TreeArray); // 利用前序遍历结果创建树
-    void preOrder();                   // 前序遍历
-    void inOrder();                    // 中序遍历
-    void postOrder();                  // 后序遍历
+    BinaryTreeNode<T> *root; // 根节点指针
+
+    void createBinaryTree(BinaryTreeNode<T> *&node);      // 创建二叉树
+    void preOrder(BinaryTreeNode<T> *node);               // 先序遍历
+    void inOrder(BinaryTreeNode<T> *node);                // 中序遍历
+    void postOrder(BinaryTreeNode<T> *node);              // 后序遍历
+    void levelOrder(BinaryTreeNode<T> *node);             // 层序遍历
+    int countNodes(BinaryTreeNode<T> *node);              // 统计结点个数
+    int countLeaves(BinaryTreeNode<T> *node);             // 统计叶子结点个数
+    int getDepth(BinaryTreeNode<T> *node);                // 获取树的深度
+    void destroy(BinaryTreeNode<T> *&node);               // 销毁二叉树
+    BinaryTreeNode<T> *copyTree(BinaryTreeNode<T> *node); // 复制二叉树
+    void swapSubtrees(BinaryTreeNode<T> *node);           // 左右子树交换
 };
-//----------------
-// 实现前面声明的函数
 
-BiTree::BiTree()
+template <typename T>
+void BinaryTree<T>::createBinaryTree(BinaryTreeNode<T> *&node)
 {
-    pos = 0;
-    strTree = "";
-}
+    string input;
+    cout << "请输入结点值(输入'NULL'表示空结点): ";
+    cin >> input;
 
-// 定义先序遍历函数
-void BiTree::preOrder() // 在public中, 提供公共接口, 在公共接口内调用私有的preorder
-{
-    preOrder(root); // 从root开始遍历
-}
-void BiTree::preOrder(BiTreeNode *t)
-{
-    if (t != nullptr)
+    // 判断是否有空结点
+    if (input == "NULL")
     {
-        cout << t->data << " ";  // 输出这个结点存储的数据
-        preOrder(t->LeftChild);  // 树的精髓在于**递归**
-        preOrder(t->RightChild); // 一直沿着左边走, 直到这个结点的左边走完了才开始走右边, 但是走了右边还是沿着左边走
-    }
-    else
+        node = nullptr;
         return;
-}
-
-// 定义中序遍历函数
-void BiTree::inOrder() // 在public中, 提供公共接口, 在公共接口内调用私有的inOrder
-{
-    inOrder(root);
-}
-void BiTree::inOrder(BiTreeNode *t)
-{
-    if (t != nullptr)
-    {
-        inOrder(t->LeftChild);
-        cout << t->data; // 与前序遍历的区别
-        inOrder(t->RightChild);
     }
-    else
+
+    // 如果不是空结点就创建新的结点存入二叉树中
+    T nodeValue;
+    istringstream(input) >> nodeValue;
+
+    if (cin.fail())
+    {
+        cerr << "输入无效, 请重新输入一个有效值: " << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        createBinaryTree(node); // 目前还未进行任何操作, 重新调用输入函数即可
         return;
+    }
+
+    // 创建结点, 并递归创建左右子树
+    node = new BinaryTreeNode<T>;
+    createBinaryTree(node->left);
+    createBinaryTree(node->right);
 }
 
-// 定义序遍历函数
-void BiTree::postOrder() // 在public中, 提供公共接口, 在公共接口内调用私有的postOrder
+template <typename T>
+void BinaryTree<T>::preOrder(BinaryTreeNode<T> *node)
 {
-    postOrder(root);
-}
-void BiTree::postOrder(BiTreeNode *t)
-{
-    if (t != nullptr)
+    if (node != nullptr)
     {
-        postOrder(t->LeftChild);
-        postOrder(t->RightChild);
-        cout << t->data << " "; // 与前序遍历的区别
+        cout << node->data << " ";
+        preOrder(node->left);
+        preOrder(node->right);
     }
-    else
-        return;
 }
 
-// 构造二叉树, 利用先序遍历结构来构造
-void BiTree::CreateTree(string TreeArray) // 共有函数, 对外接口
+template <typename T>
+void BinaryTree<T>::inOrder(BinaryTreeNode<T> *node)
 {
-    pos = 0;
-    strTree.assign(TreeArray);
-    root = CreateBiTree();
-}
-// 递归建树, 私有函数, 类内实现
-BiTreeNode *BiTree::CreateBiTree()
-{
-    BiTreeNode *t;
-    char c;
-    c = strTree[pos++];
-    if (c == '#')
-        t = nullptr;
-    else
+    if (node != nullptr)
     {
-        t = new BiTreeNode();
-        t->data = c;                    // 生成根节点
-        t->LeftChild = CreateBiTree();  // 构造该节点的右子树
-        t->RightChild = CreateBiTree(); // 构造该节点的左子树
+        inOrder(node->left);
+        cout << node->data << " ";
+        inOrder(node->right);
     }
-    return t;
+}
+
+template <typename T>
+void BinaryTree<T>::postOrder(BinaryTreeNode<T> *node)
+{
+    if (node != nullptr)
+    {
+        postOrder(node->left);
+        postOrder(node->right);
+        cout << node->data << " ";
+    }
+}
+
+template <typename T>
+void BinaryTree<T>::levelOrder(BinaryTreeNode<T> *node)
+{
+    queue<BinaryTreeNode<T> *> q;
+    q.push(node);
+
+    while (!q.empty()) // 当队列为空时就是遍历完了
+    {
+        BinaryTreeNode<T> *temp = q.front();
+        q.pop();
+        cout << temp->data << " ";
+        if (temp->left != nullptr)
+        {
+            q.push(temp->left);
+        }
+        if (temp->right != nullptr)
+        {
+            q.push(temp->right);
+        }
+    }
+}
+
+template <typename T>
+int BinaryTree<T>::countNodes(BinaryTreeNode<T> *node)
+{
+    if (node == nullptr)
+        return 0;
+    return 1 + countNodes(node->left) + countNodes(node->right); // 树的精髓在于递归, 递归算出左右子树的结点个数, 加上根结点就是整个树的结点个数
+}
+
+template <typename T>
+int BinaryTree<T>::countLeaves(BinaryTreeNode<T> *node)
+{
+    if (node == nullptr)
+        return 0;
+    if (node->left == nullptr && node->right == nullptr)
+        return 1;                                              // 左右指针均为空, 说明这个就是叶子结点
+    return countLeaves(node->left) + countLeaves(node->right); // 同样, 递归算出左右子树的叶子结点个数
+}
+
+template <typename T>
+int BinaryTree<T>::getDepth(BinaryTreeNode<T> *node)
+{
+    if (node == nullptr)
+        return 0;
+    return 1 + max(getDepth(node->left), getDepth(node->right)); // 树的深度等于左右子树深度的最大值加一, 同样, 递归是树的精髓
+}
+
+template <typename T>
+void BinaryTree<T>::destroy(BinaryTreeNode<T> *&node)
+{
+    if (node != nullptr)
+    {
+        destroy(node->left);
+        destroy(node->right);
+        delete node;
+    }
+}
+
+template <typename T>
+BinaryTreeNode<T> *BinaryTree<T>::copyTree(BinaryTreeNode<T> *node)
+{
+    if (node == nullptr)
+        return nullptr;
+    BinaryTreeNode<T> *newNode = new BinaryTreeNode<T>(node->data);
+    newNode->left = copyTree(node->left);
+    newNode->right = copyTree(node->right);
+    return newNode;
+}
+
+template <typename T>
+void BinaryTree<T>::swapSubtrees(BinaryTreeNode<T> *node)
+{
+    if (node != nullptr)
+    {
+        swap(node->left, node->right);
+        swapSubtrees(node->left);
+        swapSubtrees(node->right);
+    }
 }
